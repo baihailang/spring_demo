2efd8e5145b0ded2aa48615973c04a60622f7c89
alang <60731572+baihailang@users.noreply.github.com>
2026-09-13 11:30:42 +0800

    把扫描mapper移动到启动类
    把扫描mapper移动到启动类

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -2,12 +2,8 @@ package com.example.demo.mapper;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.entity.User;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
-
-@Mapper
 public interface UserMapper {
 
     Page<User> selectList(Page<User> page, @Param("user") User user);
